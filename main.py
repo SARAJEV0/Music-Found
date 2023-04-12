e0f682b2c697ef9cad2964e13e2a9d141b4ac4ff
Tyler Durden <vadimkuku283@gmail.com>
2023-04-12 20:05:06 +0300

    daf

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ from bs4 import BeautifulSoup
 
 url = 'https://z2.fm/'
 response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+title = soup.find('title').text
+paragraph = soup.find('p').text
+
+print('Title:', title)
+print('Paragraph:', paragraph)
+
+
+# import requests
+#import beautifulsoup4
+
+#запрос на получення даних (музики)
+#link = 'https://soundcloud.com/'
+
+#отримаєш якійсь то там контент
+#responce = requests.get(link).content
