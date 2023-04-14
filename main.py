6fc788e285e2368ab9e95a31f8cddc0e30eaa734
vadym <vadimkuku283@gmail.com>
2023-04-14 22:54:27 +0300

    Парсер для пошуку музики(не дороблений)

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as BS
 
-r = requests.get("https://stopgame.ru/news")
+r = requests.get("https://z2.fm/")
 html = BS(r.content, 'html.parser')
 
-for el in html.select(".")
+
+for el in html.select(".songs-list > .songs-list-item"):
+    title = el.select('.caption > a')
+    print(title[0].text)
+
+
+print(r) #пише чи підключився сайт до парсеру
